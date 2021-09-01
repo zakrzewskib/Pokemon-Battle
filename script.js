f9104c5f38866146e48642c94be3dff0ffc6169d
Bartosz <barzak200@gmail.com>
2021-09-01 22:47:30 +0200

    Add scores

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const playerCardsEls = document.querySelectorAll('.card--player');
+const computerCardsEls = document.querySelectorAll('.card--computer');
 const firstBattleCard = document.querySelectorAll('.card--battle')[0];
 const secondBattleCard = document.querySelectorAll('.card--battle')[1];
 
@@ -11,6 +12,7 @@ const pokemonTypes = [];
 const computerPlaysPokemon = function (src) {
   const pokemonType = pokemonTypes[0];
   setBattleCardImage(secondBattleCard, './img/cards/Pikachu.png');
+  removeCard(computerCardsEls[0]);
 };
 
 const setBattleCardImage = function (card, src) {
