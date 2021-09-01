66dd3f0aab310a69329999aafa11bb1f478c90e8
Bartosz <barzak200@gmail.com>
2021-09-01 22:41:15 +0200

    Implementing playing cards

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-const cardsEls = document.querySelectorAll('.card');
+'use strict';
 
-cardsEls.forEach(card => {
-  card.addEventListener('click', function() {
-    console.log('Attack');
-    card.classList.add('hidden');
-  })
-})
+const playerCardsEls = document.querySelectorAll('.card--player');
+const firstBattleCard = document.querySelectorAll('.card--battle')[0];
+const secondBattleCard = document.querySelectorAll('.card--battle')[1];
+
+const imgUrls = '';
+
+const pokemonTypes = [];
+
+const computerPlaysPokemon = function (src) {
+  const pokemonType = pokemonTypes[0];
+  setBattleCardImage(secondBattleCard, './img/cards/Pikachu.png');
+};
+
+const setBattleCardImage = function (card, src) {
+  card.children[0].src = src;
+  card.classList.remove('hidden');
+};
+
+const removeCard = function (card) {
+  card.classList.add('hidden');
+};
+
+const playPokemon = function (card) {
+  const imageSrc = card.children[0].src;
+  const pokemonType = pokemonTypes[card.id];
+
+  setBattleCardImage(firstBattleCard, imageSrc);
+
+  computerPlaysPokemon(imageSrc);
+};
+
+const definePokemon = function () {
+  pokemonTypes[0] = 'Electric';
+  pokemonTypes[1] = 'Fire';
+  pokemonTypes[2] = 'Grass';
+  pokemonTypes[3] = 'Psychic';
+  pokemonTypes[4] = 'Fairy';
+};
+
+const addEventListenersToPlayerCards = function () {
+  playerCardsEls.forEach(card => {
+    card.addEventListener('click', function () {
+      // setTimeout(playPokemon, 200, this);
+      playPokemon(this);
+      removeCard(this);
+    });
+  });
+};
+
+definePokemon();
+addEventListenersToPlayerCards();
