582e45f3f65456ce5f5f494883e064ed483ba974
Bartosz <barzak200@gmail.com>
2021-09-02 10:57:08 +0200

    Boths animations of the cards at the same time

--- script.js
+++ script.js
@@ -11,25 +11,72 @@ const imgUrls = '';
 
 const pokemonTypes = [];
 
-const setComputerTop = function (card) {
-  card.style.top = '128.5%';
+const tops = [];
+const length = 100;
+const firstTop = 0;
+const lastTop = -128;
+
+const topsComputer = [];
+const firstTopComputer = 0;
+const lastTopComputer = 128.5;
+
+const defineTopsForAnimation = function (top, first, last) {
+  const step = (first - last) / (length - 1);
+
+  for (let i = 0; i < length; i++) {
+    top[i] = `${first - step * i}%`;
+  }
 };
 
-const moveComputerCard = function (card) {
-  card.style.pointerEvents = 'none';
-  const left = '-22rem';
-  card.children[0].src = './img/cards/Pikachu.png';
+const setTop = function (card, player) {
+  defineTopsForAnimation(tops, firstTop, lastTop);
+  defineTopsForAnimation(topsComputer, firstTopComputer, lastTopComputer);
 
+  console.log(tops[length - 1], topsComputer[length - 1]);
+
+  let i = 0;
+  let time = 5;
+
+  let interval = setInterval(function () {
+    card.style.top = player ? tops[i] : topsComputer[i];
+    i++;
+  }, time);
+
+  setTimeout(function () {
+    clearInterval(interval);
+  }, length * time);
+};
+
+const moveCard = function (card, player) {
+  const left = player ? '0' : '-32rem';
+  card.style.left = left;
+
+  if (!player) card.children[0].src = './img/cards/Pikachu.png';
+
+  card.style.pointerEvents = 'none';
   card.style.transform = `rotate(0deg)`;
   card.style.zIndex = 0;
 
-  card.style.left = left;
-  setComputerTop(card);
+  setTop(card, player);
 };
 
+// const setComputerTop = function (card) {
+//   card.style.top = '128.5%';
+// };
+
+// const moveComputerCard = function (card) {
+//   card.style.pointerEvents = 'none';
+//   const left = '-22rem';
+
+//   card.style.transform = `rotate(0deg)`;
+//   card.style.zIndex = 0;
+
+//   card.style.left = left;
+//   setComputerTop(card);
+// };
+
 const computerPlaysPokemon = function (src) {
-  // setBattleCardImage(secondBattleCard, './img/cards/Pikachu.png');
-  moveComputerCard(computerCardsEls[0]);
+  moveCard(computerCardsEls[0], false);
 };
 
 const setBattleCardImage = function (card, src) {
@@ -41,56 +88,13 @@ const removeCard = function (card) {
   card.classList.add('hidden');
 };
 
-const tops = [];
-const length = 100;
-const firstTop = 0;
-const secondTop = -128;
-
-const defineTopsForAnimation = function () {
-  const step = (firstTop - secondTop) / (length - 1);
-
-  for (let i = 0; i < length; i++) {
-    tops.push(`${firstTop - step * i}%`);
-  }
-};
-
-const setTop = function (card) {
-  defineTopsForAnimation();
-
-  console.log(tops[0]);
-  console.log(tops[length - 1]);
-
-  let i = 0;
-  let time = 5;
-
-  let interval = setInterval(function () {
-    // console.log(card.style.top);
-    card.style.top = tops[i];
-    i++;
-  }, time);
-
-  setTimeout(function () {
-    clearInterval(interval);
-  }, length * time);
-};
-
-const moveCard = function (card) {
-  // const left = '-28rem';
-  card.style.pointerEvents = 'none';
-  const left = '0rem';
-
-  card.style.transform = `rotate(0deg)`;
-  card.style.zIndex = 0;
-
-  card.style.left = left;
-  setTop(card);
-};
-
 const playPokemon = function (card) {
   const imageSrc = card.children[0].src;
   const pokemonType = pokemonTypes[card.id];
 
   // setBattleCardImage(firstBattleCard, imageSrc);
+
+  moveCard(card, true);
 
   computerPlaysPokemon(imageSrc);
 };
@@ -109,7 +113,6 @@ const addEventListenersToPlayerCards = function () {
       // setTimeout(playPokemon, 200, this);
       playPokemon(this);
       // removeCard(this);
-      moveCard(this);
     });
   });
 };
